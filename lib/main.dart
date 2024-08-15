b70a373aff76f2eeeaeb194ceb853eaa76a703d8
Richard <rdobmk@gmail.com>
2024-08-15 11:15:32 +0200

    Match and desctructure with patterns

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import 'data.dart';
+
+void main() {
+  runApp(const DocumentApp());
+}
+
+class DocumentApp extends StatelessWidget {
+  const DocumentApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: DocumentScreen(
+        document: Document(),
+      ),
+    );
+  }
+}
+
+class DocumentScreen extends StatelessWidget {
+  final Document document;
+
+  const DocumentScreen({
+    required this.document,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final (title, :modified) = document.metadata;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Column(
+        children: [
+          Center(
+            child: Text(
+              'Last modified $modified',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
